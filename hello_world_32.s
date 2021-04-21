ba1fead75476adc17980acc206780c0006a00fec
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2021-04-21 13:40:51 +0300

    Added a program printing hello world to stderr

---
+++ hello_world_32.s
@@ -0,0 +1,17 @@
+SECTION .data
+	msg:		db 'Hello world!',0xA
+	msg_len:	equ $-msg
+
+SECTION .text
+	global _start
+
+_start:
+	mov eax, 4
+	mov ebx, 2	; Print to stderr just for a change
+	mov ecx, msg
+	mov edx, msg_len
+	int 0x80
+
+	mov eax, 1
+	xor ebx, ebx
+	int 0x80
