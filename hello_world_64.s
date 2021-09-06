b33508c71a1b603e27d1439a9cf2531b7dbc7756
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2021-09-06 10:13:36 +0300

    Added hello_world_64.s (kind of late)

---
+++ hello_world_64.s
@@ -0,0 +1,17 @@
+SECTION .rodata
+	msg:		db 'Hello world!',0xA
+	msg_len:	equ $-msg
+
+SECTION .text
+	global _start
+
+_start:
+	mov rax, 1
+	mov rdi, 1	; Print to stderr just for a change
+	mov rsi, msg
+	mov rdx, msg_len
+	syscall
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
